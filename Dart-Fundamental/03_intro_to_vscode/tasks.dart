6271b90650815fee28b6dcae61bcd123e0b78bde
userammar11 <ammarabdelsalam0@gmail.com>
2026-06-02 22:45:03 +0300

    Update tasks.dart

--- Dart-Fundamental/03_intro_to_vscode/tasks.dart
+++ Dart-Fundamental/03_intro_to_vscode/tasks.dart
@@ -7,11 +7,21 @@ void main() {
   // Print the 5 main areas of the VS Code interface.
   // YOUR CODE HERE:
 
+    print("1. Activity Bar");
+    print("2. Side Bar ");
+    print("3. Editor ");
+    print("4. Terminal ");
+    print("5. Status Bar");
+
 
   // Task 2:
   // Print your 3 favorite VS Code shortcuts and what they do.
   // Format: "Shortcut: [keys] → [action]"
   // YOUR CODE HERE:
+
+    print("Shortcut: Ctrl + Z → Undo last action");
+    print("Shortcut: Ctrl + C → Copy selected text");
+    print("Shortcut: Ctrl + V → Paste copied text");
 
 
   // Task 3:
@@ -20,11 +30,13 @@ void main() {
   // Print "Settings configured successfully!" when done.
   // YOUR CODE HERE:
 
+    print("Settings configured successfully!");
+
 
   // Task 4:
   // Open a new terminal in VS Code.
   // Type: dart --version
   // Print the version you see as a string below.
   // YOUR CODE HERE:
-
+    print("SDK version: 3.12.0 (stable) (Fri May 8 01:51:14 2026 -0700) on windows_x64");
 }
