dcf52d81d170e135532b6906da87499b98bf52de
userammar11 <ammarabdelsalam0@gmail.com>
2026-06-02 22:38:30 +0300

    Update tasks.dart

--- Dart-Fundamental/02_install_dart_sdk/tasks.dart
+++ Dart-Fundamental/02_install_dart_sdk/tasks.dart
@@ -10,12 +10,21 @@ void main() {
   // SDK Installed Successfully ✅
   // ================================
   // YOUR CODE HERE:
+  print("================================");
+  print("Welcome to Dart Programming!\nSDK Installed Successfully ✅");
+  print("================================");
 
 
   // Task 2:
   // Print the 5 key Dart commands you learned in this lesson.
   // Format: "Command: [command] → [what it does]"
   // YOUR CODE HERE:
+  print("print() : displays output on screen");
+  print("String : stores text values");
+  print("int : stores whole numbers");
+  print("void : means no return value");
+  print("main() → entry point of the program");
+
 
 
   // Task 3:
@@ -23,5 +32,5 @@ void main() {
   // Then navigate into it: cd my_first_project
   // Then run: dart run
   // Write below as a comment: What output did you see?
-  // Answer: _______________
+  // Answer: Hello world!
 }
