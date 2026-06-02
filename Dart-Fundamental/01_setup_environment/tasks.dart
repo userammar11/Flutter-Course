6829f04c546873f1cd6cbd3a1cdbab15ef59b38a
userammar11 <ammarabdelsalam0@gmail.com>
2026-06-02 22:30:19 +0300

    Implement setup tasks in tasks.dart

--- Dart-Fundamental/01_setup_environment/tasks.dart
+++ Dart-Fundamental/01_setup_environment/tasks.dart
@@ -9,12 +9,20 @@ void main() {
   // Print your name and the date you completed this setup.
   // Example output: "Setup completed by: Ahmed on 2024-01-15"
   // YOUR CODE HERE:
+    String? name = "Ammar";
+    String? date = "2026-06-02";
+    print("Setup completed by: $name on $date");
+
 
 
   // Task 2:
   // Print the 3 tools you installed today (Dart SDK, VS Code, Dart Extension).
   // Each tool on its own line.
   // YOUR CODE HERE:
+
+      print("Dart SDK");
+      print("VS Code");
+      print("Dart Extension");
 
 
   // Task 3:
@@ -23,5 +31,8 @@ void main() {
   // [✅] Dart SDK installed
   // [✅] Dart extension added
   // YOUR CODE HERE:
+    print("[✅] VS Code installed");
+    print("[✅] Dart SDK installed");
+    print("[✅] Dart extension added");
 
 }
