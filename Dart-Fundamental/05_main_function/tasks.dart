79691e1f8a822b2924745551fe8e0860919d1945
userammar11 <ammarabdelsalam0@gmail.com>
2026-06-02 22:54:39 +0300

    Update tasks.dart

--- Dart-Fundamental/05_main_function/tasks.dart
+++ Dart-Fundamental/05_main_function/tasks.dart
@@ -3,25 +3,42 @@
 // ============================================
 
 void main() {
-  // Task 1:
-  // Print "Program started" as the first line and "Program ended" as the last line.
-  // Add 3 print statements in between about yourself.
-  // YOUR CODE HERE:
+    // Task 1:
+    // Print "Program started" as the first line and "Program ended" as the last line.
+    // Add 3 print statements in between about yourself.
+    // YOUR CODE HERE:
+    print("Program started");
+    print("My name is Ammar");
+    print("My name is Ammar");
+    print("My name is Ammar");
+    print("Program ended");
 
 
-  // Task 2:
-  // Write code that prints numbers 1 to 5, each on its own line.
-  // (Write 5 separate print statements — no loops yet!)
-  // YOUR CODE HERE:
 
 
-  // Task 3:
-  // Print a simple receipt like this:
-  // ---- Receipt ----
-  // Item 1: Pen      $1
-  // Item 2: Notebook $5
-  // Total:           $6
-  // -----------------
-  // YOUR CODE HERE:
+    // Task 2:
+    // Write code that prints numbers 1 to 5, each on its own line.
+    // (Write 5 separate print statements — no loops yet!)
+    // YOUR CODE HERE:
+    print(1);
+    print(2);
+    print(3);
+    print(4);
+    print(5);
 
+
+
+    // Task 3:
+    // Print a simple receipt like this:
+    // ---- Receipt ----
+    // Item 1: Pen      $1
+    // Item 2: Notebook $5
+    // Total:           $6
+    // -----------------
+    // YOUR CODE HERE:
+    print("---- Receipt ----");
+    print("Item 1: Pen      \$1");
+    print("Item 2: Notebook \$5");
+    print("Total:           \$6");
+    print("-----------------");
 }
