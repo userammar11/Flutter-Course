efb7a6f755c2a649923bba710e5941be33789a95
userammar11 <ammarabdelsalam0@gmail.com>
2026-06-04 02:06:30 +0300

    Update tasks.dart

--- Dart-Fundamental/07_file_structure/tasks.dart
+++ Dart-Fundamental/07_file_structure/tasks.dart
@@ -7,6 +7,20 @@ void main() {
   // Create a new Dart project called "student_app" in your terminal.
   // Then print the complete file structure as text art (like in the lesson).
   // YOUR CODE HERE:
+  // student_app/
+  // │
+  // ├── .dart_tool/
+  // ├── .idea/
+  // ├── bin/
+  // │   └── student_app.dart
+  // ├── test/
+  // │   └── student_app_test.dart
+  // ├── pubspec.yaml
+  // ├── pubspec.lock
+  // ├── analysis_options.yaml
+  // ├── CHANGELOG.md
+  // ├── README.md
+  // └── .gitignore
 
 
   // Task 2:
@@ -16,6 +30,7 @@ void main() {
   // - Description
   // - Dart SDK version required
   // YOUR CODE HERE:
+  print("name: student_app\ndescription: A sample command-line application.\nsdk: ^3.12.0");
 
 
   // Task 3:
@@ -26,5 +41,11 @@ void main() {
   // - test/ → ?
   // - .gitignore → ?
   // YOUR CODE HERE:
+  print("pubspec.yaml → Contains project information, dependencies, and environment settings.");
+  print("dart → The application's entry point where execution starts.");
+  print("lib/ → Contains the main source code and reusable libraries.");
+  print("test/ → Contains unit tests and other test files for the project.");
+  print(".gitignore → Specifies files and folders that Git should ignore.");
+
 
 }
