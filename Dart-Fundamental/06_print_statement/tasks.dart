908ba7acc9636a950046b62b64afa67fcd97c2b6
userammar11 <ammarabdelsalam0@gmail.com>
2026-06-04 01:57:20 +0300

    Update tasks.dart

--- Dart-Fundamental/06_print_statement/tasks.dart
+++ Dart-Fundamental/06_print_statement/tasks.dart
@@ -9,6 +9,11 @@ void main() {
   // Example: "Hi! I'm Sara, 17 years old, from Cairo."
   // YOUR CODE HERE:
 
+  String? name = "Ammar";
+  String  city = "Ismailia";
+  int age = 17;
+  print("Hi! I'm $name, $age years old, from $city");
+
 
   // Task 2:
   // Print a formatted table like this using \t:
@@ -17,6 +22,10 @@ void main() {
   // Sara          17      Alexandria
   // Mohamed       15      Giza
   // YOUR CODE HERE:
+  print("Name\tAge\t\tCity");
+  print("$name\t$age\t\t$city");
+  print("$name\t$age\t\t$city");
+  print("$name\t$age\t\t$city");
 
 
   // Task 3:
@@ -26,7 +35,16 @@ void main() {
   // ... up to 5 x 10 = 50
   // Use string interpolation and individual print statements.
   // YOUR CODE HERE:
-
+  print("5 x 1 = ${5 * 1}");
+  print("5 x 2 = ${5 * 2}");
+  print("5 x 3 = ${5 * 3}");
+  print("5 x 4 = ${5 * 4}");
+  print("5 x 5 = ${5 * 5}");
+  print("5 x 6 = ${5 * 6}");
+  print("5 x 7 = ${5 * 7}");
+  print("5 x 8 = ${5 * 8}");
+  print("5 x 9 = ${5 * 9}");
+  print("5 x 10= ${5 * 10}");
 
   // Task 4:
   // Print this banner using special characters:
@@ -34,5 +52,5 @@ void main() {
   // *   Welcome to Dart! *
   // **********************
   // YOUR CODE HERE:
-
+  print("**********************\n* Welcome to Dart! *\n**********************");
 }
