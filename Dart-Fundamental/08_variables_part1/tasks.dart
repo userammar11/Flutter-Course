316d870d94f120713e0bbaf9a2caf233b5e02ada
userammar11 <ammarabdelsalam0@gmail.com>
2026-06-04 02:32:27 +0300

    Update tasks.dart

--- Dart-Fundamental/08_variables_part1/tasks.dart
+++ Dart-Fundamental/08_variables_part1/tasks.dart
@@ -2,6 +2,9 @@
 // 08 — Variables Part 1 | Tasks
 // ============================================
 
+
+
+
 void main() {
   // Task 1:
   // Create a "digital ID card" for yourself using variables.
@@ -9,12 +12,36 @@ void main() {
   // Print all in a nicely formatted way.
   // YOUR CODE HERE:
 
+  int ID = 001;
+  String Fname = "Ammar";
+  String Lname = "Abdelsalam";
+  String FullName = Fname + Lname;
+  int age = 18 ;
+  String school ="WE School";
+  int Grade = 1;
+  String favoriteSubject = 'ENGLISH';
+  print("===== Digital ID Card =====");
+  print("ID : $ID");
+  print("fullName: $FullName");
+  print("age : $age");
+  print("school :$school");
+  print("Grade : $Grade");
+  print("favoriteSubject : $favoriteSubject");
+  print("===========================");
+
+
 
   // Task 2:
   // Create variables for a product in a store:
   // productName, price, quantity, isAvailable
   // Print: "[productName] costs [price] EGP. In stock: [isAvailable]"
   // YOUR CODE HERE:
+  String productName = "IPhone";
+  double price = 1000;
+  bool isAvailable = true ;
+  print("productName: $productName \nprice:$price \$\nIn stock $isAvailable");
+
+
 
 
   // Task 3:
@@ -24,6 +51,15 @@ void main() {
   // After swapping: a should be "World" and b should be "Hello"
   // Hint: Use a third temporary variable!
   // YOUR CODE HERE:
+  String a = "Hello";
+  String b = "World";
+  String temp = a;
+  a =  b;
+  b = temp ;
+  print(a);
+  print(b);
+
+
 
 
   // Task 4:
@@ -33,5 +69,10 @@ void main() {
   // double Score = 97.5;
   // bool Is_Student = true;
   // YOUR CODE HERE:
+  int year2024 = 2024;
+  String mySchool = "Al Nour";
+  double score = 97.5;
+  bool isStudent = true;
+
 
 }
